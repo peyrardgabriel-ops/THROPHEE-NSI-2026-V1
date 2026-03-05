350db870956762bf07fbb1a9bd94703f43a6ae4a
Wis73OO <rob1lesage1@gmail.com>
2026-03-05 16:31:58 +0100

    Delete entity directory

--- entity/__init__.py
+++
@@ -1,5 +0,0 @@
-from .enemy import Enemy
-from .entity import Entity
-from .item import Item
-from .player import Player
-from .robot import Robot
--- entity/item_cls/iron_ingot.py
+++
@@ -1,9 +0,0 @@
-from LOGIC.entity import Item
-
-IMG_COIN = ":resources:/images/items/coinGold.png"
-
-class IronIngot(Item):
-    type = "iron ingot"
-    path_or_texture = IMG_COIN
-    def __init__(self, x, y, **kwargs):
-        super().__init__(x, y, path_or_texture = self.path_or_texture)
