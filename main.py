9b5858c2faa49ccbc9ab320c7566bae794d11e7c
Wis73OO <rob1lesage1@gmail.com>
2026-03-12 18:25:43 +0100

    Enable timings and clean up game initialization

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import arcade
 from LOGIC.menu.menu_intro.menu_intro import MenuIntro
 
-
+arcade.enable_timings()
 
 
 
 class Game(arcade.Window):
     def __init__(self):
-        super().__init__(1280, 720, "Jeu", fullscreen=True)
+        super().__init__(title="Jeu", fullscreen=True)
         self.center_window()
         self.show_view(MenuIntro(self))
 
@@ -17,4 +17,5 @@ class Game(arcade.Window):
 
 game = Game()
 arcade.run()
+
 
