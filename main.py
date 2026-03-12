ae11fd6b7c4903e9e185c1ea494f98f0056afd68
Wis7300 <rob1lesage@gmail.com>
2026-03-12 14:21:21 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import arcade
-from LOGIC.logic import GameView
+from LOGIC.menu.menu_intro.menu_intro import MenuIntro
 
-window = arcade.Window(1280, 720, "Jeu", fullscreen=True)
-window.center_window()
 
-game = GameView()
-window.show_view(game)
+
+
+
+class Game(arcade.Window):
+    def __init__(self):
+        super().__init__(1280, 720, "Jeu", fullscreen=True)
+        self.center_window()
+        self.show_view(MenuIntro(self))
+
+    
+    def switch_scene(self, view):
+        self.show_view(view)
+
+game = Game()
 arcade.run()
-
 
